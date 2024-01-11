f9817942206ed87c0ba9046b4255c34a04951a1e
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2024-01-11 17:00:51 +0100

    chore: Added next-ui ✨

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+const { nextui } = require('@nextui-org/react')
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    nextui({
+      prefix: 'ui',
+      defaultTheme: 'light',
+      addCommonColors: false,
+      themes: {},
+    })
+  ],
 }
 
